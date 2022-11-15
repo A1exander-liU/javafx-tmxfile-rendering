57eb3ae23c3cf3cea8d30fa42fb8c3b5531f0f94
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-14 21:22:56 -0800

    make Entity

---
+++ src/main/java/com/example/tiledmaptest/Entity.java
@@ -0,0 +1,33 @@
+package com.example.tiledmaptest;
+
+import javafx.geometry.Point2D;
+
+/**
+ * A game entity that has a position and a size.
+ */
+public class Entity {
+    private Point2D position;
+    private int width;
+    private int height;
+
+    /**
+     * Constructs a new game entity with the specified size. Position will be set at origin,
+     * @param width the desired width
+     * @param height the desired height
+     */
+    public Entity(final int width, final int height) {
+        this(0, 0, width, height);
+    }
+    /**
+     * Constructs a new game entity with the specified position and size.
+     * @param x the desired x position
+     * @param y the desired y position
+     * @param width the desired width
+     * @param height the desired height
+     */
+    public Entity(final int x, final int y, final int width, final int height) {
+        position = new Point2D(x, y);
+        this.width = width;
+        this.height = height;
+    }
+}
