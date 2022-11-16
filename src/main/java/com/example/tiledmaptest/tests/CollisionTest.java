7d45fc22c7992fa0e60f707f8ee4f32299af672c
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-15 18:41:52 -0800

    handle key input for player body

--- src/main/java/com/example/tiledmaptest/tests/CollisionTest.java
+++ src/main/java/com/example/tiledmaptest/tests/CollisionTest.java
@@ -4,8 +4,11 @@ import com.example.tiledmaptest.*;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import org.dyn4j.dynamics.Body;
 import org.tiledreader.*;
+
 
 /**
  * To test collision detection with tiled map object layers.
@@ -24,19 +27,49 @@ public class CollisionTest extends Application {
         TiledReader tiledReader = new FileSystemTiledReader();
         TiledMap map = tiledReader.getMap(ResourcePathMapper.getPathToResource("map-0-0.tmx", true));
         addCollidableObjects(map);
+        Entity player = new Entity(16, 16);
+        CollisionManager.getInstance().addDynamicRectangle(player);
         Group root = new Group();
         root.getChildren().add(MapRenderer.getInstance().render(map));
-        root.getChildren().add(new RectangleObject(16, 16));
+        root.getChildren().add(player.getRectangle());
         Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        scene.setOnKeyPressed(event -> {
+            float velocityX = 0f;
+            float velocityY = 0f;
+            if (event.getCode() == KeyCode.W) {
+                velocityY = -50f;
+            }
+            if (event.getCode() == KeyCode.A) {
+                velocityX = -50f;
+            }
+            if (event.getCode() == KeyCode.S) {
+                velocityY = 50f;
+            }
+            if (event.getCode() == KeyCode.D) {
+                velocityX = 50f;
+            }
+            if (event.getCode() == KeyCode.SHIFT) {
+                velocityX *= 1.5;
+                velocityY *= 1.5;
+            }
+            if (velocityX != 0 || velocityY != 0) {
+                player.getBody().setLinearVelocity(velocityX, velocityY);
+            }
+        });
         GameLoop gameLoop = new GameLoop() {
             @Override
             public void tick(final float secondsSinceLastFrame) {
                 CollisionManager.getInstance().updateWorld(secondsSinceLastFrame);
+                for (Body body: CollisionManager.getInstance().getBodies()) {
+                    Entity entity = (Entity) body.getUserData();
+                    entity.setX((float) body.getTransform().getTranslationX());
+                    entity.setY((float) body.getTransform().getTranslationY());
+                }
             }
         };
         gameLoop.start();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     /**
      * Launches the application.
